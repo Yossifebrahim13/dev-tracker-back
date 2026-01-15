63b3d5b3996b874b9f1d63ebf24a96cdfd713294
Yossifebrahim <yossifebrahim76@example.com>
2026-01-15 19:08:31 +0200

    Fix app.js path, setup package.json for deployment

--- src/utils/sendmailer.js
+++ src/utils/sendmailer.js
@@ -1,27 +1,29 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
-
 const sendMail = async (to, subject, html) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT || 587),
-        secure: Number(process.env.SMTP_PORT) === 465, 
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS,
-        },
-    });
-    
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT || 587),
+    secure: Number(process.env.SMTP_PORT) === 465,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
+    logger: true,
+    debug: true,
+  });
 
-    
-    const info = await transporter.sendMail({
-        from: process.env.FROM_EMAIL,
-        to,
-        subject,
-        html,
-    });
-    return info;
+  const info = await transporter.sendMail({
+    from: process.env.FROM_EMAIL,
+    to,
+    subject,
+    html,
+  });
+  return info;
 };
 
 module.exports = sendMail;
