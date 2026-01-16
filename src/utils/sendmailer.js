7c2153f7c003799721ec2f018f3e1cc6b1a19b60
Yossifebrahim <yossifebrahim76@example.com>
2026-01-16 18:38:15 +0200

    Install resend and ignore env file

--- src/utils/sendmailer.js
+++ src/utils/sendmailer.js
@@ -1,6 +1,6 @@
 const SibApiV3Sdk = require('sib-api-v3-sdk');
 require("dotenv").config();
-
+//comment
 const client = SibApiV3Sdk.ApiClient.instance;
 client.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
 
