1d2cd2dcd24f2448e2a0ebf553b8422c4586da8c
Yossifebrahim <yossifebrahim76@example.com>
2026-02-03 05:18:00 +0200

    avoid nosql injection

---
+++ src/middlewares/noBody.middleware.js
@@ -0,0 +1,10 @@
+const noBodyAllowed = (req, res, next) => {
+  if (req.body && Object.keys(req.body).length > 0) {
+    return res.status(400).json({
+      message: "Request body is not allowed for this endpoint",
+    });
+  }
+  next();
+};
+
+module.exports = noBodyAllowed;
--- src/modules/auth/routes/project.routes.js
+++ src/modules/auth/routes/project.routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { createProjectDev, completedProjectDev, getAllArchivedProjects, getAllProjects, deleteProject, deleteAllProjects } = require('../controllers/projectcontroller/project');
 const { protect } = require('../../../middlewares/auth.middleware');
 const { deleteOneProject } = require('../repositories/project.repository');
+const noBodyAllowed = require('../../../middlewares/noBody.middleware');
 const projectRouter = express.Router();
 projectRouter.post('/dev/projectdev/createprojectdev' , protect , createProjectDev); 
 projectRouter.patch('/dev/projectdev/archiveprojectdev/:id' , protect , completedProjectDev)
 projectRouter.get('/dev/projectdev/archivedprojects/history'  , protect , getAllArchivedProjects)
 projectRouter.get('/dev/projectdev/projects' , protect , getAllProjects )
 projectRouter.delete('/dev/projectdev/deleteProject/:id' , protect , deleteProject)
-projectRouter.delete('/dev/projectdev/clearhistory' , protect , deleteAllProjects)
+projectRouter.delete('/dev/projectdev/clearhistory' , protect , noBodyAllowed , deleteAllProjects)
 module.exports = {projectRouter}
